a6745a35c136af698d80bc5d8c6a9b785cf34be0
skwnddp <7883121@naver.com>
2026-04-09 13:36:02 +0900

    npm 일부 패키지 통합 및 획일화 및 app, main, index 세부 로직 통일화
    Co-Authored-By: 최지혜 <choijihye295@gmail.com>

--- Skeleton-project/src/App.vue
+++ Skeleton-project/src/App.vue
@@ -2,24 +2,31 @@
 import { RouterView } from 'vue-router';
 // 기존에 설정하신 경로 그대로 유지합니다.
 import Header from '@/sides/header-page.vue';
+// import JsonTest from './components/json-test.vue';
 </script>
 
 <template>
   <div class="app-container min-vh-100 bg-white">
     <Header />
-
     <main class="container py-4">
       <RouterView />
     </main>
   </div>
 </template>
+<style scoped>
+/* #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        padding: 20px; */
 
-<style>
 .app-background {
   background-color: #f2f2f2;
   min-height: 100vh;
   display: flex;
   justify-content: center;
+}
+
+header {
+  line-height: 1.5;
 }
 
 .mobile-container {
--- Skeleton-project/src/router/index.js
+++ Skeleton-project/src/router/index.js
@@ -6,6 +6,7 @@ import profileSettings from '@/components/ProfileSettingsView.vue';
 // import startPage from '@/components/start-page.vue';
 import startPage from '@/components/StartView.vue';
 import transactionHistory from '@/components/transaction-history.vue';
+// import JsonTest from "../components/json-test.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,13 +16,24 @@ const router = createRouter({
     { path: '/addTransaction', component: addTransaction },
     { path: '/profileSettings', component: profileSettings },
     { path: '/transactionHistory', component: transactionHistory },
+    { path: '/history', component: transactionHistory },
     {
       path: '/add/:type',
       name: 'add-transaction',
       component: addTransaction,
       props: true,
     },
+
+    {
+      path: "/test1",
+      name: "test1",
+      component: () => import("../components/json-test.vue"),
+    },
+    {
+      path: "/test2",
+      name: "test2",
+      component: () => import("../components/json-test2.vue"),
+    }
   ],
 });
-
 export default router;

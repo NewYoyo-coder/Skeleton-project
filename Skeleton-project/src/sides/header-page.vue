bb3829527d9d51f03d024cc134239828d00d9672
seokyunLee <seokyunlee.dev@gmail.com>
2026-04-09 20:39:29 +0900

    header 스타일 수정

--- Skeleton-project/src/sides/header-page.vue
+++ Skeleton-project/src/sides/header-page.vue
@@ -1,43 +1,42 @@
 <template>
-  <header class="py-3">
-    <div class="container">
-      <nav
-        class="d-flex align-items-center flex-nowrap text-nowrap overflow-x-auto"
+  <header class="py-3 mx-auto w-100" style="max-width: 600px">
+    <div class="d-flex justify-content-between align-items-center px-4">
+      <router-link
+        to="/mainDashboard"
+        class="text-black text-decoration-none fs-5 fw-bold"
       >
-        <router-link
-          to="/mainDashboard"
-          class="text-black text-decoration-none fs-1 fs-md-1 fw-bold me-auto"
+        가계부
+      </router-link>
+
+      <div class="d-flex align-items-center gap-2 gap-md-3">
+        <a
+          href="#"
+          class="text-black text-decoration-none"
+          style="font-size: 0.9rem"
+          data-bs-toggle="modal"
+          data-bs-target="#monthModal"
+          @click.prevent
         >
-          가계부
+          날짜선택
+        </a>
+
+        <span class="text-secondary" style="font-size: 0.9rem">|</span>
+
+        <router-link
+          to="/transactionHistory"
+          class="text-black text-decoration-none"
+          style="font-size: 0.9rem"
+        >
+          거래내역
         </router-link>
 
-        <div class="d-flex align-items-center gap-2 gap-md-3">
-          <a
-            href="#"
-            class="text-black text-decoration-none ms-2 ms-md-4"
-            data-bs-toggle="modal"
-            data-bs-target="#monthModal"
-            @click.prevent
-          >
-            날짜선택
-          </a>
-
-          <span class="text-black">|</span>
-          <router-link
-            to="/transactionHistory"
-            class="text-black text-decoration-none"
-          >
-            거래내역
-          </router-link>
-
-          <router-link
-            to="/profileSettings"
-            class="text-black text-decoration-none ms-2 ms-md-3"
-          >
-            <i class="fa-solid fa-gear fs-5 fs-md-4"></i>
-          </router-link>
-        </div>
-      </nav>
+        <router-link
+          to="/profileSettings"
+          class="text-black text-decoration-none ms-1"
+        >
+          <i class="fa-solid fa-gear fs-5 text-dark"></i>
+        </router-link>
+      </div>
     </div>
 
     <div class="modal fade" id="monthModal" tabindex="-1" aria-hidden="true">

6012a78aa3d3c0c70f2f5a71754b263f53cd7412
skwnddp <7883121@naver.com>
2026-04-09 11:07:59 +0900

    index, css 다크모드 로직 개선 및 finia store 함수 반응성 개선
    Co-Authored-By: Kangshang Heo <200321165+kangshang123@users.noreply.github.com>
    Co-Authored-By: seokyunLee <seokyunlee.dev@gmail.com>

--- Skeleton-project/src/router/index.js
+++ Skeleton-project/src/router/index.js
@@ -1,15 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import addTransaction from '@/components/add-transaction.vue';
 import Maindashboard from '@/components/main-dashboard.vue';
-import profileSettings from '@/components/profile-settings.vue';
-import startPage from '@/components/start-page.vue';
+// import profileSettings from '@/components/profile-settings.vue';
+import profileSettings from '@/components/ProfileSettingsView.vue';
+// import startPage from '@/components/start-page.vue';
+import startPage from '@/components/StartView.vue';
 import transactionHistory from '@/components/transaction-history.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: startPage},
-    { path: '/mainDashboard', component: Maindashboard},
+    { path: '/', component: startPage },
+    { path: '/mainDashboard', component: Maindashboard },
     { path: '/addTransaction', component: addTransaction },
     { path: '/profileSettings', component: profileSettings },
     { path: '/transactionHistory', component: transactionHistory },
